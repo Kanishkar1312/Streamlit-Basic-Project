b4f45e9a02ae2eb8755c669f590808694776eda3
Kanishkar1312 <kani1312skmr@gmail.com>
2025-05-04 03:31:04 +0530

    Add files via upload

---
+++ Constant.py
@@ -0,0 +1,29 @@
+#Table_names
+Category = "Category"
+Competition = "Competition"
+Complex = "Complex"
+Venue = "Venue"
+Competitor_Ranking = "CompetitorRanking"
+Ranking = "Ranking"
+Competitor = "Competitor"
+
+#SQL Types
+SQL_INTEGER = "INTEGER"
+SQL_FLOAT = "REAL"
+SQL_STRING = "TEXT"
+
+#Constants For SQL QUERIES
+CREATEDATABASE = "CREATE DATABASE IF NOT EXISTS SPORTSRADARTENNIS"
+USEDATABASE = "USE SPORTSRADARTENNIS"
+CREATETABLE = "CREATE TABLE IF NOT EXISTS"
+INSERTINTO = "INSERT INTO"
+FOREIGNKEY = "FOREIGN KEY"
+REFERENCES = "REFERENCES"
+VALUES = "VALUES"
+
+SPORTSRADARTENNIS ="SPORTSRADARTENNIS"
+
+#FilePath Constants
+CompetitionsJSONPath = "C:/Users/Kanishkar Rajeswari/Desktop/StreamLit/JsonData/competitions.json"
+ComplexJSONPath = "C:/Users/Kanishkar Rajeswari/Desktop/StreamLit/JsonData/complexes.json"
+DoubleCompetitorJSONPath = "C:/Users/Kanishkar Rajeswari/Desktop/StreamLit/JsonData/double_competitors_rankings.json"
